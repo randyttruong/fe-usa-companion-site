891dabf07c636933113e905e7d6aefd335e6b172
jasonhu27 <60557121+jasonhu27@users.noreply.github.com>
2024-05-12 22:00:05 -0500

    updated companion site

--- fe-usa-companion-site/src/pages/ContactPage/ContactPage.js
+++ fe-usa-companion-site/src/pages/ContactPage/ContactPage.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./ContactPage.css"
 import Banner from "../../components/Banner/Banner"
 import NavBar from "../../components/NavBar/NavBar"
 import TopBar from '../../components/TopBar/TopBar'
+import ComponentEntry from '../../components/ComponentEntry/ComponentEntry';
+import ComponentSubmit from '../../components/ComponentSubmit/ComponentSubmit';
 
 function ContactPage() { 
+
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+
+    // handleAddressChange(): 
+    // Helper function for setting 
+    // the address of the contact page.
+    const handleAddressChange = (str) => {
+        setAddress(str);
+        console.log(`This is str ${str}`)
+    }
+
+    // handlePhoneChange(): 
+    // Helper function for setting 
+    // the phone number of the contact page.
+    const handlePhoneChange = (str) => {
+        setPhone(str);
+        console.log(`This is str ${str}`)
+    }
+
+    // handleEmailChange(): 
+    // Helper function for setting 
+    // the email of the contact page.
+    const handleEmailChange = (str) => {
+        setEmail(str);
+        console.log(`This is str ${str}`)
+    }
+
+    // handleSubmit
+    // Helper function for sending form data to the API
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{ 
+        const resp = await fetch("http://localhost:8000/update-homepage", 
+        {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({address, phone, email}),
+        }); 
+
+        if (resp.ok) { 
+            const data = resp.json();
+            console.log("Successfully updated contact page");
+            console.log(data);
+        } else { 
+            throw new Error("Failed to update contact page"); 
+        }
+
+        } catch (err) { 
+            console.error(err);
+        }
+    }
+        
     return (
         <>
             <div className="contact-container">
@@ -12,7 +71,25 @@ function ContactPage() {
                 <Banner />
                 <div className="body"> 
                     <NavBar />
-                    <div className="contact-body">
+                    <div className="contact-content">
+                    <form onSubmit={handleSubmit}>
+                        <ComponentEntry 
+                            label="Address"
+                            value={address} 
+                            fn={handleAddressChange}
+                        />
+                        <ComponentEntry
+                            label="Phone Number"
+                            value={phone}
+                            fn={handlePhoneChange}
+                        /> 
+                        <ComponentEntry
+                            label="Email"
+                            value={email}
+                            fn={handleEmailChange}
+                        /> 
+                        <ComponentSubmit />
+                    </form>
                     </div>
                 </div>
             </div>
